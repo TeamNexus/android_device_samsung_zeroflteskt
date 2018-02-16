5fd57b95df01713642cddea01f2cbe681cf50712
Lukas0610 <mail@lukasberger.at>
2018-02-16 12:05:09 +0100

    zeroflteskt: added support for AICP
    Change-Id: I7be065ade8007e629cc807aa7451358be7293b97

---
+++ aicp.mk
@@ -0,0 +1,16 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zeroflteskt/full_zeroflteskt.mk)
+
+
+# Enhanced NFC
+# $(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/aicp/configs/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="zeroflteskt" \
+    TARGET_DEVICE="zeroflte"
+
+PRODUCT_NAME := aicp_zeroflteskt
+PRODUCT_DEVICE := zeroflteskt
